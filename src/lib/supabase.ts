deeb89329a17d2054784cd41fcb5393a92ca9d98
hello-itss-me <melkeeee46@gmail.com>
2025-10-04 10:07:00 +0300

    Updated vite.config.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '../types/database'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://0ec90b57d6e95fcbda19832f.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODE1NzQsImV4cCI6MTc1ODg4MTU3NH0.9I8-U0x86Ak8t2DGaIk0HfvTSLsAyzdnz-Nw00mMkKw'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://arqxycghkorgvgdgkfnm.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFycXh5Y2doa29yZ3ZnZGdrZm5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1MTE0MTEsImV4cCI6MjA3NTA4NzQxMX0.NiuWNqULeirtPsgc-SxKir4ht7K496E_QJLB3HtYydo'
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
